7326fd178bee6377e9d3e865387ec5222cbddaef
Czjena <149963386+Czjena@users.noreply.github.com>
2026-06-04 13:42:55 +0200

    feat : add eq repo

--- src/main/java/com/campusgear/demo/repository/EquipmentRepository.java
+++ src/main/java/com/campusgear/demo/repository/EquipmentRepository.java
@@ -4,5 +4,5 @@ import com.campusgear.demo.entity.EquipmentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EquipmentRepository extends JpaRepository<EquipmentEntity, Long> {
-    // Tutaj kolega będzie dopisywał metody do wyszukiwania, np. findByDeviceType(...)
+
 }
